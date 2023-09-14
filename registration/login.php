c7494d299a7cd5a7a0773b557183bee9f4090398
Erzmal3011 <141235877+Erzmal3011@users.noreply.github.com>
2023-09-14 10:23:10 +0800

    redirect admin to homepage
    redirect admin to homepage

--- registration/login.php
+++ registration/login.php
@@ -28,7 +28,7 @@ else {
 	echo'<div class="w3-center w3-container" style="width:400px; margin:auto">';
 	echo "<center><br><br><div class='w3-center w3-container w3-red w3-margin w3-padding'><b><br>Wrong User Id or Password !!!<br><br></b>";
 	echo'</div>';
-	echo '<br><br><br><a class="w3-text-blue" href="../mainMenu.php"><b>Try Again</b></a>';
+	echo '<br><br><br><a class="w3-text-blue" href="loginPage.html"><b>Try Again</b></a>';
 	}
 
     // Close the database connection
